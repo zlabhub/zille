382d5b71063793d594b6a7d070fc6fb71c84abe9
沈赟杰 <shenyunjie@znrank.com>
2025-05-08 00:30:18 +0800

    update

--- packages/vite/src/code.ts
+++ packages/vite/src/code.ts
@@ -1,6 +1,6 @@
 export interface ZilleVitePluginCodeOptions {
   eager?: boolean,
-  directory?: string,
+  controllerDirectory?: string,
   controllerSuffix?: string,
   controllerMetaSuffix?: string,
   service?: boolean,
@@ -10,7 +10,7 @@ export interface ZilleVitePluginCodeOptions {
  * 生成注入代码
  * @param options 
  * @param options.eager 是否急加载 default: false
- * @param options.directory 目录 default: controllers
+ * @param options.controllerDirectory 目录 default: controllers
  * @param options.controllerSuffix 控制器后缀 default: .controller.tsx
  * @param options.controllerMetaSuffix 控制器元数据后缀 default: .meta.ts
  * @param options.service 是否生成服务 default: false
@@ -20,7 +20,7 @@ export interface ZilleVitePluginCodeOptions {
 export function makeInjectionCode(options: ZilleVitePluginCodeOptions = {}) {
   const {
     eager = false,
-    directory = 'controllers',
+    controllerDirectory = 'controllers',
     controllerSuffix = '.controller.tsx',
     controllerMetaSuffix = '.meta.ts',
     service = false,
@@ -31,21 +31,21 @@ export function makeInjectionCode(options: ZilleVitePluginCodeOptions = {}) {
   const serviceCode = service ? makeServiceCode(serviceDirectory) : '';
 
   return `import { addRoute, setWindowEager, setWindowBaseURL, addControllerMetadata } from '@zille/react';
-const controllers = import.meta.glob('./${directory}/**/*${controllerSuffix}', {
+const controllers = import.meta.glob('./${controllerDirectory}/**/*${controllerSuffix}', {
   eager: ${eagerValue},
   import: 'default',
 });
-const metas = import.meta.glob('./${directory}/**/*${controllerMetaSuffix}', {
+const metas = import.meta.glob('./${controllerDirectory}/**/*${controllerMetaSuffix}', {
   eager: true,
   import: 'default',
 });
 Object.entries(controllers).forEach(([path, fn]) => {
-  const url = path.slice('./${directory}'.length, '${controllerSuffix}'.length * -1).replace(/\\/index$/, '') || '/';
+  const url = path.slice('./${controllerDirectory}'.length, '${controllerSuffix}'.length * -1).replace(/\\/index$/, '') || '/';
   const route = url.replace(/\\[\\.\\.\\.([^\\]]+)\\]/g, '**:$1').replace(/\\[([^\\]]+)\\]/g, ':$1');
   addRoute(route, fn);
 });
 Object.entries(metas).forEach(([path, meta]) => {
-  const url = path.slice('./${directory}'.length, '${controllerMetaSuffix}'.length * -1).replace(/\\/index$/, '') || '/';
+  const url = path.slice('./${controllerDirectory}'.length, '${controllerMetaSuffix}'.length * -1).replace(/\\/index$/, '') || '/';
   const route = url.replace(/\\[\\.\\.\\.([^\\]]+)\\]/g, '*$1').replace(/\\[([^\\]]+)\\]/g, ':$1');
   addControllerMetadata(meta, new meta(route));
 });
